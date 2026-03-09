b80307c03ca70b7e088c470657c991ec3eb2989e
Pradhan121 <sm.pradhan101@gmail.com>
2026-03-09 19:14:24 +0530

    new code added

---
+++ model/userAuth.js
@@ -0,0 +1,10 @@
+
+const mongoose = require('mongoose')
+
+const userAuth = new mongoose.Schema({
+    userName: String,
+    email: String,
+    password: String
+})
+
+module.exports = mongoose.model('user',userAuth)
